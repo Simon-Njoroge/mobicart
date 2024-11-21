95a1c035f66481e8a178919d67cba779b1e03f2c
simon njoroge <mukirisimon22@gmail.com>
2024-11-21 15:56:00 +0300

    inital

---
+++ frontend/src/containers/HomeContainer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from '../components/home'
+function HomeContainer() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  )
+}
+
+export default HomeContainer
---
+++ frontend/src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HomeContainer from '../containers/HomeContainer'
+function Homepage() {
+  return (
+   <div>
+    <HomeContainer/>
+   </div>
+  )
+}
+
+export default Homepage
